4dead70129f28e2a6a7f999c3a13631ec1fa6319
Abdullah Debo <abd.valancia2@gmail.com>
2024-08-30 23:06:54 +0300

    initiate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'ui/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyB7KOKKDIC5rUBvThNZ0K-HOEkKspbnqDY',
+      appId: 'plantio-app-4b746',
+      messagingSenderId: '129805791304',
+      projectId: 'plantio-app-4b746',
+    ),
+  );
   runApp(const MyApp());
 }
 
