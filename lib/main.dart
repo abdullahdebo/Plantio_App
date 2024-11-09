a97e112d0c555cba82df19aced81dec436d37a9f
Abdullah Debo <54190547+abdullahdebo@users.noreply.github.com>
2024-11-09 11:00:15 +0300

    Add storage bucket

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,14 @@ import 'ui/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: FirebaseOptions(
-      //   apiKey: 'AIzaSyB7KOKKDIC5rUBvThNZ0K-HOEkKspbnqDY',
-      //   appId: 'plantio-app-4b746',
-      //   messagingSenderId: '129805791304',
-      //   projectId: 'plantio-app-4b746',
-      // ),
-      );
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyB7KOKKDIC5rUBvThNZ0K-HOEkKspbnqDY',
+      appId: 'plantio-app-4b746',
+      messagingSenderId: '129805791304',
+      projectId: 'plantio-app-4b746',
+      storageBucket: 'plantio-app-4b746.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
